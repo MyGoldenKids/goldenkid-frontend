675ce4044a0ce145758fb84befc7a392d8c5f922
kimgiraffe <kimgiraffe@naver.com>
2024-01-18 16:10:39 +0900

    feat: 마이페이지, 로그아웃 메뉴 #1
    로그인 한 경우 마이페이지, 로그아웃 메뉴
    추후 로그인 여부에 따라 회원가입, 로그인 메뉴 전환 구현 필요

--- vue-project/src/components/common/HeaderComponent.vue
+++ vue-project/src/components/common/HeaderComponent.vue
@@ -2,12 +2,23 @@
 
 <template>
     <div class="header">
+        <div class="member">
+                <!-- 로그인 했을 때 -->
+                <div class="member-sub">
+                    <div>
+                        <a href="#">MY PAGE</a> 
+                    </div>
+                    <div>
+                        <a href="#">LOGOUT</a>
+                    </div>
+                </div>
+            </div>
         <div class="logo">
-            <a href="/"><img src="@/assets/img/logo.png" /></a>
+            <router-link to="/"><img src="@/assets/img/logo.png" /></router-link>
         </div>
         <div class="nav">
             <div class="menu-item"><a href="#">금쪽일기</a></div>
-            <div class="menu-item"><a href="#">꿀팁</a></div>
+            <div class="menu-item"><router-link to="/article/list">꿀팁</router-link></div>
             <div class="menu-item"><a href="#">진단</a></div>
             <div class="menu-item"><a href="#">주변병원</a></div>
         </div>
@@ -58,4 +69,26 @@ a:hover {
 .menu-item:hover {
     background-image: url(../../assets/img/sticker3.png);
 }
+
+.member {
+    display: grid;
+}
+.member-sub {
+    justify-self: end;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0.313rem;
+}
+.member-sub div a{
+    padding: 0.4rem 0.625rem;
+    border: 4px dotted #AD9478;
+    text-align: center;
+    border-radius: 10px;
+}
+.member-sub div a:hover {
+    background-color: #AD9478;
+    color: #fff;
+    transition-duration: 0.5s;
+}
+
 </style>
