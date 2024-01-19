74d866c8fb1fbb196eac8992f976ab6e738d074c
Hunnibs <cqqudgjs@hufs.ac.kr>
2024-01-19 15:37:40 +0900

    feat: 자가진단 view #5
    view 생성 & 라우팅 설정

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainViewVue from "@/views/MainView.vue";
+import SurveyViewVue from "@/views/survey/SurveyView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/",
-            component: MainViewVue,
+            component: MainViewVue
         },
+        {
+            path: "/survey",
+            component: SurveyViewVue
+        }
     ],
 });
 
