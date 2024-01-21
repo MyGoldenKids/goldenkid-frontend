69639022d407c79a105d467fdaf079eb35c28f7f
kimgiraffe <kimgiraffe@naver.com>
2024-01-21 16:14:00 +0900

    feat: VCalendar 라이브러리 기본 설정 #7

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,5 +10,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VCalendar);
 
 app.mount("#app");
