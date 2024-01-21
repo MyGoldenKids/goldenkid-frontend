889b8e1798834745a8168fe283faec825524bc33
kimgiraffe <kimgiraffe@naver.com>
2024-01-21 13:38:10 +0900

    feat: survey header

---
+++ vue-project/src/components/survey/SurveyHeaderComponent.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+    <!-- 진단 -->
+    <div class="survey-header">
+        <h1>진단</h1>
+    </div>
+</template>
+
+<style scoped>
+.survey-header {
+    padding: 40px 0;
+}
+.survey-header h1 {
+    color: #665031;
+    font-size: 30px;
+    font-weight: 700;
+    text-align: center;
+    padding-bottom: 35px;
+    border-bottom: 1px solid #ad9478;
+}
+</style>
--- vue-project/src/views/survey/SurveyView.vue
+++ vue-project/src/views/survey/SurveyView.vue
@@ -1,9 +1,11 @@
 <script setup>
+    import SurveyHeaderComponent from "@/components/survey/SurveyHeaderComponent.vue";
     import QuestionComponent from "@/components/survey/QuestionComponent.vue"
 </script>
 
 <template>
     <div>
+        <SurveyHeaderComponent></SurveyHeaderComponent>
         <QuestionComponent></QuestionComponent>
     </div>
 </template>
