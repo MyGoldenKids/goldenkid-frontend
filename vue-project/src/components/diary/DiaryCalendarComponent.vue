ed6ae9bf0d147cc8b2099b3b03f723a59cd135e2
hunnibs <hunnibs97@gmail.com>
2024-01-21 17:14:46 +0900

    feat: 상단 컴포넌트 분리
    주어진 그리드에 맞춰 우선적으로 레이아웃을 짜기 위해 상단 컴포넌트를 분리

--- vue-project/src/components/diary/DiaryCalendarComponent.vue
+++ vue-project/src/components/diary/DiaryCalendarComponent.vue
@@ -11,8 +11,6 @@ const attrs = ref([
 </script>
 
 <template>
-    <!--다이어리 홈 위젯 상단 -->
-    <div class="content-top">
         <!-- 캘린더 -->
         <div class="calendar">
             <VCalendar
@@ -23,59 +21,14 @@ const attrs = ref([
                 :attributes="attrs"
             ></VCalendar>
         </div>
-        <!-- 미리보기 -->
-        <div class="preview">
-            <img src="../../assets/img/sticker5.png" class="sticker1" />
-            <img src="../../assets/img/sticker5.png" class="sticker2" />
-            <p class="preview-temp-text">
-                캘린더 날짜를 누르면 해당 일자 게시글 미리보기를 여기서 볼 수
-                있어요 :D
-            </p>
-        </div>
-    </div>
 </template>
 
 <style scoped>
-.content-top {
-    /* background-color: aquamarine; */
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    margin-bottom: 1.2rem;
-}
 
 /* 캘린더 & 캘린더 미리보기 */
 .calendar {
     width: 70%;
     height: 12.5rem;
     margin: 0 auto;
-}
-.preview {
-    width: 70%;
-    height: 12.5rem;
-    margin: 0 auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #89b9ad;
-    border-radius: 20px;
-    position: relative;
-}
-.preview-temp-text {
-    width: 40%;
-    font-size: 0.8rem;
-}
-.sticker1 {
-    width: 7.5rem;
-    position: absolute;
-    top: 0;
-    left: -1.875rem;
-    transform: rotate(-25deg);
-}
-.sticker2 {
-    width: 6.25rem;
-    position: absolute;
-    bottom: 0;
-    right: -1.563rem;
-    transform: rotate(-25deg);
 }
 </style>
--- vue-project/src/views/diary/DiaryHomeView.vue
+++ vue-project/src/views/diary/DiaryHomeView.vue
@@ -3,16 +3,27 @@ import DiaryHeaderComponent from "@/components/diary/DiaryHeaderComponent.vue";
 import DiaryNavComponent from "@/components/diary/DiaryNavComponent.vue";
 import DiaryProfileComponent from "@/components/diary/DiaryProfileComponent.vue";
 import DiaryCalendarComponent from "@/components/diary/DiaryCalendarComponent.vue";
+import DiaryPreviewComponent from "@/components/diary/DiaryPreviewComponent.vue";
 </script>
 
 <template>
-    <DiaryHeaderComponent></DiaryHeaderComponent>
     <div class="diary">
         <DiaryNavComponent></DiaryNavComponent>
-        <div class="diary-main">
-            <DiaryProfileComponent></DiaryProfileComponent>
-            <div class="diary-content">
-                <DiaryCalendarComponent></DiaryCalendarComponent>
+        <div>
+            <DiaryHeaderComponent></DiaryHeaderComponent>
+            <div class="diary-main">
+                <DiaryProfileComponent></DiaryProfileComponent>
+                <div class="diary-content">
+                    <!--다이어리 홈 위젯 상단 -->
+                    <div class="content-top">
+                        <DiaryCalendarComponent></DiaryCalendarComponent>
+                        <DiaryPreviewComponent></DiaryPreviewComponent>
+                    </div>
+                    <!--다이어리 홈 위젯 하단 -->
+                    <div class="content-bottom">
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -40,4 +51,18 @@ import DiaryCalendarComponent from "@/components/diary/DiaryCalendarComponent.vu
     padding: 0.313rem 1.2rem;
 }
 
+/* 컨텐츠 상위 영역 */
+.content-top {
+    /* background-color: aquamarine; */
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin-bottom: 1.2rem;
+}
+
+/* 컨텐츠 하위 영역 */
+.content-bottom {
+    /* background-color: cadetblue; */
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+}
 </style>
