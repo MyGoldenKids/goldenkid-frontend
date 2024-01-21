89882d6c61ce625dd1a6d1b8060256e3032cfa1f
kimgiraffe <kimgiraffe@naver.com>
2024-01-21 14:02:15 +0900

    feat: 다이어리 헤더 #4

---
+++ vue-project/src/views/diary/DiaryHomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import DiaryHeaderComponent from "@/components/diary/DiaryHeaderComponent.vue";
+</script>
+
+<template>
+    <div>
+        <DiaryHeaderComponent></DiaryHeaderComponent>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
