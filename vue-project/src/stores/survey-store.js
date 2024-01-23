addd0674445a0f40e2ca09f05bfd2d71c282e520
Hunnibs <cqqudgjs@hufs.ac.kr>
2024-01-23 09:24:37 +0900

    chore: survey 값 상태저장 파일

--- vue-project/src/stores/survey-store.js
+++ vue-project/src/stores/survey-store.js
@@ -4,18 +4,25 @@ import { ref } from "vue";
 export const useSurveyStore = defineStore("survey-store", () => {
     const status = ref(false);
     const category = ref(0);
+    const parentScore = ref(-1);
+    const childScore = ref(-1);
     const parentComment = ref(0);
     const childComment = ref(0);
 
     function $reset(){
+        parentScore.value = -1;
+        childScore.value = -1;
         parentComment.value = 0;
         childComment.value = 0;
     }
 
     return {
+        $reset,
         category,
         status,
-        parentComment, 
+        parentScore, 
+        childScore,
+        parentComment,
         childComment,
     };
 });
