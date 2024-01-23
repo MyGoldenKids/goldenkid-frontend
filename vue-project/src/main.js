c482baf745dfc7e62ea417adb9cb2b0a6f202d63
kimgiraffe <kimgiraffe@naver.com>
2024-01-23 13:09:21 +0900

    feat: axios 기본 설정

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,15 +1,18 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import VCalendar from 'v-calendar';
-import 'v-calendar/style.css';
+import VCalendar from "v-calendar";
+import "v-calendar/style.css";
 
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(VCalendar);
+app.use(axios);
 
 app.mount("#app");
+app.config.globalProperties.axios = axios; // axios 전역 설정
