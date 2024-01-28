b2f858adb400d335503a7ebae3932c36602835f1
kimgiraffe <kimgiraffe@naver.com>
2024-01-28 14:43:33 +0900

    fix: 회원관리 axios #41
    회원 정보 수정 시 현재 비밀번호 입력 추가
    탈퇴 시 로그인한 사용자 정보 초기화 및 메인페이지로 이동

--- vue-project/src/components/member/MemberMyPageComponent.vue
+++ vue-project/src/components/member/MemberMyPageComponent.vue
@@ -5,11 +5,14 @@ import {
     modifyMemberDetail,
     deleteMember,
 } from "@/api/member";
-
+import { useMemberStore } from "@/stores/member-store";
+import router from "@/router";
+const store = useMemberStore();
 const memberNo = sessionStorage.getItem("memberNo");
 const memberDetail = ref({
     memberNo: memberNo,
     password: "",
+    newPassword: "",
     nickname: "",
     phoneNumber: "",
 });
@@ -38,22 +41,48 @@ function submitForm() {
         }
     );
 }
+
+function signOut(memberNo) {
+    deleteMember(memberNo);
+    store.isLoggedIn = false; // 로그인 여부 false로 갱신
+    store.memberInfo = ""; // 로그인한 사용자 정보 초기화
+    sessionStorage.clear(); // 세션 storage 초기화
+    router.push("/"); // 탈퇴 후 메인 페이지로 이동
+}
 </script>
 
 <template>
-    <div>{{ memberDetail }}</div>
-    <div>
-        <form @submit.prevent="submitForm">
-            <input type="password" v-model="memberDetail.password" />
-            <br />
-            <input type="text" v-model="memberDetail.nickname" />
-            <br />
-            <input type="tel" v-model="memberDetail.phoneNumber" />
-            <br />
-            <button type="submit">수정</button>
-        </form>
-    </div>
-    <button @click="deleteMember(memberNo)">탈퇴</button>
+    <div>닉네임: {{ memberDetail.nickname }}</div>
+    <div>연락처: {{ memberDetail.phoneNumber }}</div>
+    <form @submit.prevent="submitForm">
+        <input
+            type="password"
+            placeholder="현재 비밀번호"
+            v-model="memberDetail.password"
+        />
+        <br />
+        <input
+            type="password"
+            placeholder="새 비밀번호"
+            v-model="memberDetail.newPassword"
+        />
+        <br />
+        <input
+            type="text"
+            placeholder="닉네임"
+            v-model="memberDetail.nickname"
+        />
+        <br />
+        <input
+            type="tel"
+            placeholder="연락처"
+            v-model="memberDetail.phoneNumber"
+        />
+        <br />
+        <button type="submit">수정</button>
+    </form>
+
+    <button @click="signOut(memberNo)">탈퇴</button>
 </template>
 
 <style scoped></style>
