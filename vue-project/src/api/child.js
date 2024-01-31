b2fb5d909cfeabfb9fd34ea7496947b45ff6ff22
kimgiraffe <kimgiraffe@naver.com>
2024-01-31 14:14:51 +0900

    feat: 아이 관련 axios #48
    아이 정보 목록 axios
    아이 정보 수정, 삭제 axios

--- vue-project/src/api/child.js
+++ vue-project/src/api/child.js
@@ -2,7 +2,7 @@ import { instance } from "./axios";
 
 // 아이 정보 등록
 const registerChild = async (data) => {
-    instance
+    await instance
         .post("child/register", data.value)
         .then((response) => {
             console.log(response.data);
@@ -14,7 +14,22 @@ const registerChild = async (data) => {
 
 // 아이 상세 정보 조회
 const getChildInfo = async (childId, success, fail) => {
-    instance.get(`child/detail/${childId}`).then(success).catch(fail);
+    await instance.get(`child/detail/${childId}`).then(success).catch(fail);
 };
 
-export { registerChild, getChildInfo };
+// 아이 정보 조회
+const getChildList = async (memberId, success, fail) => {
+    await instance.get(`child/list/${memberId}`).then(success).catch(fail);
+}
+
+// 아이 정보 수정
+const updateChildInfo = async(child, success, fail) => {
+    await instance.put("child/modify", child).then(success).catch(fail);
+}
+
+// 아이 정보 삭제
+const deleteChildInfo = async(childId, success, fail) => {
+    await instance.delete(`child/delete/${childId}`).then(success).catch(fail);
+}
+
+export { registerChild, getChildInfo, getChildList, updateChildInfo, deleteChildInfo };
