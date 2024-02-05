ece2e52ee092b9dd2a57821a596925d6135c36d6
kimgiraffe <kimgiraffe@naver.com>
2024-02-05 10:23:02 +0900

    feat: 댓글 작성 axios #64

--- vue-project/src/api/comment.js
+++ vue-project/src/api/comment.js
@@ -5,4 +5,12 @@ const getCommentByArticleId = async (articleId, success, fail) => {
     await instance.get(`comment/list/${articleId}`).then(success).catch(fail);
 };
 
-export { getCommentByArticleId };
+// 댓글 작성
+const writeComment = async (articleId, comment, success, fail) => {
+    await instance
+        .post(`comment/write/${articleId}`, comment)
+        .then(success)
+        .catch(fail);
+};
+
+export { getCommentByArticleId, writeComment };
