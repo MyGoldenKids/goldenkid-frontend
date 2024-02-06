a5c21a99fd106465c44a97757a3c51387b7d5898
정소영 <so2043@naver.com>
2024-02-06 18:03:32 +0900

    Feat: 마이페이지 조회 레이아웃 적용 #76

--- vue-project/src/components/member/MemberMyPageComponent.vue
+++ vue-project/src/components/member/MemberMyPageComponent.vue
@@ -11,8 +11,8 @@ const store = useMemberStore();
 const memberNo = sessionStorage.getItem("memberNo");
 const memberDetail = ref({
     memberNo: memberNo,
-    password: "",
-    newPassword: "",
+    password: "", 
+    newPassword: "", 
     nickname: "",
     phoneNumber: "",
 });
@@ -37,7 +37,7 @@ function submitForm() {
             // 회원정보 수정 시 수정한 닉네임으로 업데이트
             sessionStorage.setItem("nickname", memberDetail.value.nickname);
             store.memberInfo.nickname = memberDetail.value.nickname;
-            alert("회원정보가 변경되었습니다.")
+            alert("회원정보가 변경되었습니다.");
         },
         () => {
             alert("비밀번호가 일치하지 않습니다.");
@@ -55,37 +55,62 @@ function signOut(memberNo) {
 </script>
 
 <template>
-    <div>닉네임: {{ memberDetail.nickname }}</div>
-    <div>연락처: {{ memberDetail.phoneNumber }}</div>
-    <form @submit.prevent="submitForm">
-        <input
-            type="password"
-            placeholder="현재 비밀번호"
-            v-model="memberDetail.password"
-        />
-        <br />
-        <input
-            type="password"
-            placeholder="새 비밀번호"
-            v-model="memberDetail.newPassword"
-        />
-        <br />
-        <input
-            type="text"
-            placeholder="닉네임"
-            v-model="memberDetail.nickname"
-        />
-        <br />
-        <input
-            type="tel"
-            placeholder="연락처"
-            v-model="memberDetail.phoneNumber"
-        />
-        <br />
-        <button type="submit">수정</button>
-    </form>
-
-    <button @click="signOut(memberNo)">탈퇴</button>
+    <!-- 엄마 -->
+    <div class="mypage-grid">
+        <div class="mom-box">
+            <img src="@/assets/img/bear-profile.png" alt="금쪽프로필" />
+            <p>{{ memberDetail.nickname }}</p>
+            <p>{{ memberDetail.phoneNumber }}</p>
+        </div>
+    </div>
+    <!-- 버튼 -->
+    <div class="mypage-grid">
+        <div class="btn-box">
+            <button @click="regist">내정보 수정</button>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.mypage-grid {
+    width: 50%;
+    margin: 0 auto;
+}
+/* 엄마 정보 */
+.mom-box {
+    width: 50%;
+    margin: 0 auto;
+    background-color: #89b9ad;
+    text-align: center;
+    border-radius: 1.87rem;
+    color: #fff;
+    padding: 0.625rem 0;
+}
+.mom-box img {
+    width: 50%;
+}
+.mom-box p {
+    margin: 0.625rem 0;
+    font-size: 1.1rem;
+}
+
+/* 버튼 */
+.btn-box {
+    width: 50%;
+    margin: 0 auto;
+}
+.btn-box button {
+    width: 100%;
+    border: none;
+    height: 2.5rem; 
+    border: 3px solid #ad9478;
+    background-color: inherit;
+    border-radius: 0.625rem;
+    margin-top: 0.625rem;
+}
+.btn-box button:hover {
+    background-color: #ad9478;
+    color: #fff;
+    transition-duration: 0.5s;
+}
+</style>
