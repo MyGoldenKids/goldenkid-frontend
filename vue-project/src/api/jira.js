42e2b6220c8b418dc75797b7e3799313b7b449e0
kimgiraffe <kimgiraffe@naver.com>
2024-02-07 08:40:37 +0900

    feat: Jira 스프린트 axios #71
    생성, 조회, 수정, 상태 변경, 삭제 axios 구현

---
+++ vue-project/src/api/jira.js
@@ -0,0 +1,46 @@
+import { instance } from "./axios";
+
+// 스프린트 생성
+const createSprint = async (data, success, fail) => {
+    await instance.post("jira/sprint", data).then(success).catch(fail);
+};
+
+// 스프린트 조회
+const getSprintList = async (memberId, success, fail) => {
+    await instance
+        .get(`jira/sprint/list/${memberId}`)
+        .then(success)
+        .catch(fail);
+};
+
+// 스프린트 상태 변경
+const changeSprintStatus = async (sprintId, data, success, fail) => {
+    await instance
+        .patch(`jira/sprint/status/${sprintId}`, data)
+        .then(success)
+        .catch(fail);
+};
+
+// 스프린트 수정
+const modifySprint = async (sprintId, data, success, fail) => {
+    await instance
+        .put(`jira/sprint/modify/${sprintId}`, data)
+        .then(success)
+        .catch(fail);
+};
+
+// 스프린트 삭제
+const deleteSprint = async (sprintId, memberId, success, fail) => {
+    await instance
+        .delete(`jira/sprint/delete/${sprintId}`, memberId)
+        .then(success)
+        .catch(fail);
+};
+
+export {
+    createSprint,
+    getSprintList,
+    changeSprintStatus,
+    modifySprint,
+    deleteSprint,
+};
