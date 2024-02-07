2aee0b17d91847e3cf184bfa542afeb32f07c58d
kimgiraffe <kimgiraffe@naver.com>
2024-02-07 09:31:02 +0900

    feat: Jira 스토리 axios #113
    생성, 상세조회, 스프린트별 조회, 수정, 상태 변경, 삭제 axios 구현

--- vue-project/src/api/jira.js
+++ vue-project/src/api/jira.js
@@ -37,10 +37,61 @@ const deleteSprint = async (sprintId, memberId, success, fail) => {
         .catch(fail);
 };
 
+// 스토리 생성
+const createStory = async (data, success, fail) => {
+    await instance.post("jira/story/create", data).then(success).catch(fail);
+};
+
+// 스프린트별 스토리 조회
+const getStoryList = async (sprintId, memberId, success, fail) => {
+    await instance
+        .get(`jira/story/list/${sprintId}/member/${memberId}`)
+        .then(success)
+        .catch(fail);
+};
+
+// 스토리 상세 조회
+const getStoryDetail = async (storyId, success, fail) => {
+    await instance
+        .get(`jira/story/detail/${storyId}/member/${memberId}`)
+        .then(success)
+        .catch(fail);
+};
+
+// 스토리 수정
+const modifyStory = async (storyId, data, success, fail) => {
+    await instance
+        .put(`jira/story/modify/${storyId}`, data)
+        .then(success)
+        .catch(fail);
+};
+
+// 스토리 상태 변경
+const changeStoryStatus = async (storyId, data, success, fail) => {
+    await instance
+        .patch(`jira/story/status/${storyId}`, data)
+        .then(success)
+        .catch(fail);
+};
+
+// 스토리 삭제
+const deleteStory = async (storyId, memberId, success, fail) => {
+    await instance
+        .delete(`jira/story/delete/${storyId}/member/${memberId}`)
+        .then(success)
+        .catch(fail);
+};
+
 export {
     createSprint,
     getSprintList,
     changeSprintStatus,
     modifySprint,
     deleteSprint,
+    createStory,
+    getStoryList,
+    getStoryDetail,
+    modifyStory,
+    changeStoryStatus,
+    deleteStory,
 };
