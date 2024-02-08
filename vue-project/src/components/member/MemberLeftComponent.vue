5a91df4671261d42606efb9c66eab6f7905fa0ff
Hunnibs <cqqudgjs@hufs.ac.kr>
2024-02-08 14:58:29 +0900

    feat: login view 구현 #145
    left component가 왼쪽에 붙지 못하는 점을 해결해야합니다.

---
+++ vue-project/src/components/member/MemberLeftComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import logo from '@/assets/img/mainbear.png';
+
+const bgImage = ref(logo);
+</script>
+
+<template> 
+    <div class="wrap-left" :style="{ backgroundImage: `url(${bgImage})`}">
+        <button class="center-button">금쪽이 해방일지</button>
+    </div>
+</template>
+
+<style scoped>
+.wrap-left {    
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-position: left bottom;
+}
+
+.center-button {
+    transform: translateY(-2000%);
+    background-color: #fff8f2;
+    width: 10.63rem;
+    height: 3.125px;
+    font-size: 1.25rem;
+}
+</style>
--- vue-project/src/views/member/MemberLoginView.vue
+++ vue-project/src/views/member/MemberLoginView.vue
@@ -1,11 +1,21 @@
 <script setup>
 import MemberLoginComponent from "@/components/member/MemberLoginComponent.vue";
+import MemberLeftComponent from "@/components/member/MemberLeftComponent.vue";
 </script>
 
 <template>
-    <div>
+    <div class="user-wrap">
+        <MemberLeftComponent></MemberLeftComponent>
         <MemberLoginComponent></MemberLoginComponent>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.user-wrap {
+    width: 100%;
+    /* background-color: blanchedalmond; */
+    display: grid;
+    grid-template-columns: minmax(200px, 40%) 60%;
+    height: 100vh;
+}
+</style>
