5573643a504de358ee45b1aff5cedc0e1fcf44cb
kjy0349 <kjy0349@naver.com>
2024-02-11 20:15:26 +0900

    feature: sprint, story 생성 추가

--- vue-project/src/api/member.js
+++ vue-project/src/api/member.js
@@ -1,67 +1,67 @@
-import { instance } from "@/api//axios";
+import { instance } from "@/api/axios";
 import { ref } from "vue";
 
 const hasInput = ref(false);
 const message = ref("");
 
 const checkId = (member_id) => {
-    if (member_id.trim() !== "") {
-        hasInput.value = true;
-        instance
-            .post("member/idcheck/" + member_id)
-            .then((response) => {
-                if (response.data.data === 0) {
-                    message.value = "중복된 아이디가 존재합니다!";
-                } else {
-                    message.value = "사용가능한 아이디입니다.";
-                }
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    } else {
-        hasInput.value = false;
-        message.value = "";
-    }
+  if (member_id.trim() !== "") {
+    hasInput.value = true;
+    instance
+      .post("member/idcheck/" + member_id)
+      .then((response) => {
+        if (response.data.data === 0) {
+          message.value = "중복된 아이디가 존재합니다!";
+        } else {
+          message.value = "사용가능한 아이디입니다.";
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  } else {
+    hasInput.value = false;
+    message.value = "";
+  }
 };
 
 const signup = (data, success, fail) => {
-    instance.post("member/signup", data.value).then(success).catch(fail);
+  instance.post("member/signup", data.value).then(success).catch(fail);
 };
 
 // 로그인
 const login = (data, success, fail) => {
-    instance.post("member/login", data.value).then(success).catch(fail);
+  instance.post("member/login", data.value).then(success).catch(fail);
 };
 
 // 회원정보 조회
 const getMemberDetail = (member_no, success, fail) => {
-    instance
-        .get("member/detail/" + member_no)
-        .then(success)
-        .catch(fail);
+  instance
+    .get("member/detail/" + member_no)
+    .then(success)
+    .catch(fail);
 };
 
 // 회원정보 수정
 const modifyMemberDetail = (member, success, fail) => {
-    instance.put("member/modify", member.value).then(success).catch(fail);
+  instance.put("member/modify", member.value).then(success).catch(fail);
 };
 
 // 회원 탈퇴
 const deleteMember = (member_no, success, fail) => {
-    instance
-        .put("member/signout/" + member_no)
-        .then(success)
-        .catch(fail);
+  instance
+    .put("member/signout/" + member_no)
+    .then(success)
+    .catch(fail);
 };
 
 export {
-    hasInput,
-    message,
-    checkId,
-    signup,
-    login,
-    getMemberDetail,
-    modifyMemberDetail,
-    deleteMember,
+  hasInput,
+  message,
+  checkId,
+  signup,
+  login,
+  getMemberDetail,
+  modifyMemberDetail,
+  deleteMember,
 };
