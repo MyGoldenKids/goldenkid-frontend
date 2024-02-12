f21741f7d35f44989d0fbdfd7ecbecb223012b80
Hunnibs <hunnibs97@gmail.com>
2024-02-13 00:13:50 +0900

    feat: router link to main #145

--- vue-project/src/components/member/MemberLeftComponent.vue
+++ vue-project/src/components/member/MemberLeftComponent.vue
@@ -1,13 +1,19 @@
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import logo from '@/assets/img/mainbear.png';
 
+const router = useRouter();
 const bgImage = ref(logo);
+
+const goMain = () => {
+    router.push("/");
+};
 </script>
 
 <template> 
     <div class="wrap-left" :style="{ backgroundImage: `url(${bgImage})`}">
-        <button class="center-button">금쪽이 해방일지</button>
+        <button @click="goMain" class="center-button">금쪽이 해방일지</button>
     </div>
 </template>
 
