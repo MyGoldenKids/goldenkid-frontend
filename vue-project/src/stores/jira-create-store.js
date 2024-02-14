5c78285e57bd7fa765f65c2ce1c690b599de0ab4
dkdo1205 <dkdo1205@gmail.com>
2024-02-14 18:03:18 +0900

    hotfix: 금쪽플래너 버그 수정

--- vue-project/src/stores/jira-create-store.js
+++ vue-project/src/stores/jira-create-store.js
@@ -3,7 +3,7 @@ import {ref} from 'vue'
 
 export const useJiraCreateStore = defineStore('jira-create-store', () => {
     const sprintInfo = ref({}); // sprint: title, startDate, endDate
-    const storyList = ref([undefined, undefined, undefined, undefined]); // story: storyContent, storyPoint 
+    const storyList = ref([]); // story: storyContent, storyPoint 
     const totalStoryPoint = ref(0);
 
     function $reset(){
