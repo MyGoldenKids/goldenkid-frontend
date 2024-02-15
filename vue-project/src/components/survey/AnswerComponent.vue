126205bab5a77c84c9c0df57089e1c5cc6e29ac7
kimgiraffe <kimgiraffe@naver.com>
2024-02-15 10:35:12 +0900

    feat: 진단 결과 페이지에서 금쪽플래너 홈으로 이동 #218

--- vue-project/src/components/survey/AnswerComponent.vue
+++ vue-project/src/components/survey/AnswerComponent.vue
@@ -1,7 +1,13 @@
 <script setup>
 import { useSurveyStore } from "@/stores/survey-store";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const store = useSurveyStore();
+
+const goJira = () => {
+    router.push("/jira/home");
+};
 </script>
 
 <template>
@@ -18,6 +24,10 @@ const store = useSurveyStore();
         <p>
             {{ store.childComment }}
         </p>
+        <div class="go-signup">
+            PLAN 기록하기
+            <button @click="goJira">GO</button>
+        </div>
     </div>
     <div v-else>
         <p>{{ store.parentComment }}</p>
@@ -30,5 +40,24 @@ p {
     width: 70%;
     margin: 0 auto;
     line-height: 1.5rem;
+}
+
+.go-signup {
+    text-align: center;
+    padding: 2rem 0 0 1rem;
+    font-size: 1.1rem;
+}
+.go-signup button {
+    border: none;
+    border-radius: 30%;
+    background-color: #89B9AD;
+    color: #fff8f2;
+    font-size: 1.1rem;
+    padding: 0.313rem 0.625rem;
+}
+
+.go-signup button:hover {
+    background-color: #e2bcb1;
+    transition: 0.5s;
 }
 </style>
